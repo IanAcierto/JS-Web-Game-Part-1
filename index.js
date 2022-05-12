9e1ac7ebdbf2a8bcc66f123c82f5d4a65e7474a1
IanAcierto <101164364+IanAcierto@users.noreply.github.com>
2022-05-12 14:44:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(image, left, bottom){
+  let object = document.createElement('img')
+  object.src = image
+  object.style.position = 'fixed'
+  object.style.left = left + 'px'
+  object.style.bottom = bottom + 'px'
+  document.body.append(object)
+}
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', '450', '150')
+newImage('assets/tree.png', '200', '300')
+newImage('assets/pillar.png', '350', '100')
+newImage('assets/well.png', '500', '425')
+function newItem(newImage){
+  let item = newImage();
+  item.addEventListener(dbclick,function(){
+    item.remove()
+  });
+  return item;
+}
+
+let sword = newItem(newImage('assets/sword.png', '500', '450'))
+let shield = newItem(newImage('assets/shield.png', '165', '185'))
+let staff = newItem(newImage('assets/staff.png', '600', '100'))
+//i couldn't figure out newItem for the life of meuiylet sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
+/*
+
+newobjects
+___________
+assets/tree.png (200px from the left, 300px from the bottom)
+assets/pillar.png (350px from the left, 100px from the bottom)
+assets/crate.png (150px from the left, 200px from the bottom)
+assets/well.png (500px from the left, 425px from the bottom)
+new items
+__________
+assets/shield.png (165px from the left, 185px from the bottom)
+assets/staff.png (600px from the left, 100px from the bottom)
+*/
